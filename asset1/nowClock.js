3c3491e9b324a634792c71bf12911a163c3312cb
Junya Nakajima <nakatuy@gmail.com>
2021-01-25 12:15:21 +0900

    Add files via upload

---
+++ asset1/nowClock.js
@@ -0,0 +1,26 @@
+var patch
+
+
+function set2fig(num) {
+  // 桁数が1桁だったら先頭に0を加えて2桁に調整する
+  var ret;
+  if( num < 10 ) { ret = "0" + num; }
+  else { ret = num; }
+  return ret;
+}
+function showClock() {
+  var nowTime = new Date();
+  var nowHour = set2fig( nowTime.getHours() );
+  var nowMin  = set2fig( nowTime.getMinutes() );
+  var nowSec  = set2fig( nowTime.getSeconds() );
+  var msg = nowHour + " h " + nowMin + " m " + nowSec + " s ";
+  document.getElementById("RealtimeClockArea").innerHTML = msg;
+  Pd.send('minute', [nowMin]);
+}
+setInterval('showClock()',1000);
+
+
+$.get('./patches/myPatch.pd', function(patchStr) {
+  patch = Pd.loadPatch(patchStr)
+  Pd.start()
+})
